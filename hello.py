970d4f137b3b7ecd5470d1dc910731a5f9467a0a
Jianbin Wang <826258349@qq.com>
2018-07-28 11:25:41 +0800

    3d

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
-#Time:2018-07-20 
-#Name:wangjianbin
-#Project:learning machine code
+# Time:2018-07-20
+# Name:wangjianbin
+# Project:learning machine code
 import numpy
+import tensorflow as tf
+
 print('let\'s go')
+
